82a4fea51524ea6f7fb0638e8b7eff207cb893e8
Marijke Drenth <drenth.marijke@googlemail.com>
2021-12-15 20:11:34 +0200

    Finished MVP

--- models/traveller.js
+++ models/traveller.js
@@ -7,19 +7,21 @@ Traveller.prototype.getJourneyStartLocations = function() {
 };
 
 Traveller.prototype.getJourneyEndLocations = function () {
-
+ return this.journeys.map(journey => journey.endLocation)
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
-
+  return this.journeys.filter(journey => journey.transport === transport)
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
-
+  return this.journeys.filter(journey => journey.distance > minDistance)
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
-
+ return this.journeys.reduce((runningTotal, journey) =>{
+   return runningTotal + journey.distance
+ },0)
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
